0b32a0d20eea268526710049e6e8baea01a8664d
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2025-12-28 18:21:17 +0530

    New changes are added

--- BlogWebsite/src/App.jsx
+++ BlogWebsite/src/App.jsx
@@ -1,13 +1,41 @@
-
-import './App.css'
+import { useState, useEffect, useLayoutEffect } from 'react';
+import {useDispatch} from 'react-redux';
+import './App.css';
+import authservice from './appwrite/auth';
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'; 
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      authservice.currentUser()
+      .then((userData) =>{
+        if (userData) {
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
   
- return (
-    <>
-     <h1>A BlogApp with appwrite</h1>
-    </>
-  )
+
+ return !loading? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+    <div className='w-full block'>
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+  </div>
+ ) : null
+
+
 }
 
 export default App
