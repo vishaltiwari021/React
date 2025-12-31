98c8e71debc42c5d614bbbbee2289cf5f2b9f34a
vishaltiwari021 <vishaltiwarivt021@gmail.com>
2025-12-31 19:48:16 +0530

    Routes

--- BlogWebsite/src/main.jsx
+++ BlogWebsite/src/main.jsx
@@ -11,19 +11,58 @@ const router  = createBrowserRouter([
   {
     path:'/',
     element: <App />,
-    children:[
-      {
-        path:'/',
-        element:<Home />
-      },
-      {
-        path:'/login',
-        element:(
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        )
-      }
+    children: [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/login",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Login />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/signup",
+            element: (
+                <AuthLayout authentication={false}>
+                    <Signup />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/all-posts",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AllPosts />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/add-post",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <AddPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/edit-post/:slug",
+            element: (
+                <AuthLayout authentication>
+                    {" "}
+                    <EditPost />
+                </AuthLayout>
+            ),
+        },
+        {
+            path: "/post/:slug",
+            element: <Post />,
+        },
     ]
   }
 ])
